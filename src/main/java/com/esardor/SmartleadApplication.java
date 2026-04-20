f737164c325397cbcef7c654aac46b0830dd6345
sardoregamberdiev <sardor.egamberdiyev@gmail.com>
2026-04-20 10:46:05 +0800

    chore: initialize project structure and configuration

---
+++ src/main/java/com/esardor/SmartleadApplication.java
@@ -0,0 +1,13 @@
+package com.esardor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartleadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartleadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esardor/SmartleadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esardor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartleadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
